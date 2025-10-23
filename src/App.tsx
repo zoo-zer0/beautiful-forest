9d3379e9064d31047697a6eae5fc81e595b8f95d
joo <122433286+kepten31415926@users.noreply.github.com>
2025-10-24 03:54:41 +0900

    remove intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import StadiumInteractive from './components/StadiumInteractive/StadiumInteractive.tsx';
-import Intro from './components/Intro.tsx';
 function App() {
   
 
   return (
     <div>
-      <Intro />
       <StadiumInteractive />
     </div>
   );
